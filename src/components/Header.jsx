c2e7c7a1912a52fe6e3ad01cb78a287649211385
Frogalove <143967544+Frogalove@users.noreply.github.com>
2024-01-13 20:43:05 +0100

    eerst commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import'./Header.css'
+
+
+
+function Header () {
+  return (
+    <>
+      <div className='outer-container'>
+        <div className='inner-container'>
+          <span>
+          </span>
+        </div>
+      </div>
+    </>
+
+  )
+}
+
+export default Header
