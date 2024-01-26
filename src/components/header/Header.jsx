4ec18e17826c0e5ffde52fd1521331c91aad4323
Frogalove <143967544+Frogalove@users.noreply.github.com>
2024-01-26 09:57:10 +0100

    toch bedacht een andere shop te maken, verwijderd/toegevoegd een andere pics

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,16 @@
 import'./Header.css'
 
-
-
 function Header () {
   return (
     <>
+      <header>
       <div className='outer-container'>
         <div className='inner-container'>
           <span>
           </span>
         </div>
       </div>
+      </header>
     </>
 
   )
