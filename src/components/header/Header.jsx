a65a66bbd6791289eec891ecb8cdfa3769d3b2c2
Frogalove <143967544+Frogalove@users.noreply.github.com>
2024-01-27 23:41:44 +0100

    met header css gewerkt

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,23 @@
+import LogoImg from '/src/assets/logo.svg'
 import'./Header.css'
 
 function Header () {
-  return (
-    <>
-      <header className="header">LOGO & NAV</header>
-    </>
+  return <header className="header">
+  <div className='container'>
+    <div className='header_row'>
+      <div className='header_logo'>
+        <img src={LogoImg} alt="Logo"/>
+        <span>Fashion</span>
+      </div>
+      <div className='header_nav'>NAV</div>
 
-  )
+
+    </div>
+  </div>
+
+
+  </header>;
+
 }
 
-export default Header
+export default Header;
